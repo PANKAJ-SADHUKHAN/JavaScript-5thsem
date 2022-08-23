23ebbb225f0219fc89dd3f04488ad30699a975ef
PANKAJ SADHUKHAN <pankajsadhukhan243@gmail.com>
2022-08-23 21:45:08 +0530

    Create AddTwoNumbers.js

---
+++ AddTwoNumbers.js
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    My JavaScript
+  </head>
+  <body>
+    <h1>AddTwoNumbers</h1>
+    <script>
+        var a=5;
+        var b=6;
+        var c=a+b;
+        document.write("Value of a is:" +a);
+        document.write("Value of b is:" +b);
+        document.write("The result is:" +c);
+    </script>
+  </body>
+</html>
