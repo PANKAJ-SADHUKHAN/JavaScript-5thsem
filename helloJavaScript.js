d2581f5738ad28ad0fc8b67f5876807af43295b4
PANKAJ SADHUKHAN <79303687+PANKAJ-SADHUKHAN@users.noreply.github.com>
2022-08-23 21:19:43 +0530

    create hello Java Script

---
+++ helloJavaScript.js
@@ -0,0 +1,11 @@
+<html>
+  <head>
+     Displaying a message
+  </head>
+  <body>
+    <h1>My Java Script code </h1>
+    <script>
+      document.write("Hello Java Script");
+    </script>
+  </body>
+</html>
