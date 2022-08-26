3ee345133c469cf4ea01cb9f3af020545f14580a
PANKAJ SADHUKHAN <pankajsadhukhan243@gmail.com>
2022-08-26 13:53:19 +0530

    Create addTwoNumbers using prompt box.java

---
+++ addTwoNumbers using prompt box.java
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Displaying a Message</title>
+	</head>
+	<body>
+		<h1>Displaying a Message</h1>
+		<script>
+			var a=Number(prompt("Enter 1st number"));
+			var b=Number(prompt("Enter 2nd number"));
+			var c=a+b;
+			document.write("1st value is: "+a);
+			document.write("<br>2nd value is: "+b);
+			document.write("<br>Sum is: "+c);
+		</script>
+	</body>
+</html>
